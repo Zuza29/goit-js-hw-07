1fc14c26ee2c4b1cffa19219b2efaa2cd4e84ab7
Zuza <zusapphire@gmail.com>
2022-09-23 21:26:33 +0200

    Homework update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+
+
+const showOriginalSize = (event, imageSrc) => {
+    event.preventDefault();
+    const instance = window.basicLightbox.create(`
+    <div class="modal">
+        <img src=${imageSrc} />
+    </div>
+`); 
+    instance.show();
+    document.addEventListener("keydown", (event) => {
+        const key = event.key;
+        if (basicLightbox.visible() && key === "Escape") {
+            instance.close();
+            console.log("closed")
+        }
+    });
+};
+
+galleryItems.forEach((item, index) => {
+    gallery.insertAdjacentHTML("beforeend", `<div class="gallery__item"> 
+  <a class="gallery__link" href=${item.original}> 
+    <img 
+      class="gallery__image" 
+      src=${item.preview} 
+      data-source=${item.original}
+      alt=${item.description}
+    /> 
+  </a> 
+</div> 
+`);
+    const galleryLink = document.querySelector(`body > div.gallery > div:nth-child(${index + 1}) > a`);
+    galleryLink.addEventListener("click", (event) => { showOriginalSize(event, item.original) });
+    
+});
+
+
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const mappedGallery = galleryItems
+    .map(
+        (image) =>
+            `<a class="gallery__item" href="${image.original}">
+    <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+    </a>`)
+    .join("");
+
+gallery.insertAdjacentHTML("beforeend", mappedGallery);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom",
+});
 
 console.log(galleryItems);
